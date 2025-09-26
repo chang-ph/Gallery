eda4b839d1a6fc432910b906ff7a445dc5260164
PanHongChang <phchang@qq.com>
2025-09-26 14:32:43 +0800

    add comments to model file

--- StatisticalRethinkingJulia/centered-oceanic-tool-complexity/main.jl
+++ StatisticalRethinkingJulia/centered-oceanic-tool-complexity/main.jl
@@ -1,4 +1,9 @@
-# auto-generated
+"""
+This model is from https://github.com/vectorly-ai/Gallery/tree/main/StatisticalRethinkingJulia/centered-oceanic-tool-complexity.
+The data it needs is also in that directory (https://github.com/vectorly-ai/Gallery/blob/main/StatisticalRethinkingJulia/centered-oceanic-tool-complexity/data.csv).
+If you don't provide any data in the workflow, that data will be used by default.
+If you want to provide data, it should be provided in the same format as the default data and attached to the workflow.
+"""
 
 # ## Data
 
--- StatisticalRethinkingJulia/non-identifiable/main.jl
+++ StatisticalRethinkingJulia/non-identifiable/main.jl
@@ -1,4 +1,6 @@
-# auto-generated
+"""
+This model is from https://github.com/vectorly-ai/Gallery/tree/main/StatisticalRethinkingJulia/non-identifiable
+"""
 
 # ## Data
 

8c9683415edb0ef3f4ee59c71a8672efe0396fa4
PanHongChang <phchang@qq.com>
2025-10-23 11:48:53 +0800

    update

--- StatisticalRethinkingJulia/africa/script.jl
+++ StatisticalRethinkingJulia/africa/script.jl
@@ -4,9 +4,9 @@ using AbstractMCMC
 flow = Coinfer.ServerlessBayes.current_workflow()
 m = flow.model(flow.parsed_data...)
 
-parallel_algorithm = eval(m.setting['sampling_params']['parallel_algorithm'])
-iteration_count = m.setting['sampling_params']['iteration_count']
-num_chains = m.setting['sampling_params']['num_chains']
+parallel_algorithm = eval(m.setting["sampling_params"]["parallel_algorithm"])
+iteration_count = m.setting["sampling_params"]["iteration_count"]
+num_chains = m.setting["sampling_params"]["num_chains"]
 
 Coinfer.ServerlessBayes.sample(
     StableRNG(Int(floor(time()))),

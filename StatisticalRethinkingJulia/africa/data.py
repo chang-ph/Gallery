c227a18ef7e710f62a3e3230ac7d743395dc8f9c
PanHongChang <phchang@qq.com>
2025-10-23 13:47:30 +0800

    update

--- StatisticalRethinkingJulia/africa/data.py
+++ StatisticalRethinkingJulia/africa/data.py
@@ -12,8 +12,8 @@ import numpy as np
 from io import StringIO
 from Coinfer import current_workflow
 
-def interpret_data(data):
-    df = pd.read_csv(StringIO(data), sep=';')
+def interpret_data(data: bytes):
+    df = pd.read_csv(StringIO(data.decode('utf-8')), sep=';')
     df['log_gdp'] = np.log(df['rgdppc_2000'])
     df = df.dropna()
     return [df['log_gdp'], df['rugged'], df['cont_africa']]
